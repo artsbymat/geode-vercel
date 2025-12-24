3bef1e4fd274ea6f50f858afd102bb32ab7e8e73
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-12-24 21:57:49 +0700

    update script

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ tar -C /tmp -xzf go${GO_VERSION}.linux-amd64.tar.gz
 export PATH=/tmp/go/bin:$PATH
 
 go version
-go run ./cmd/geode/main.go build -dir docs
-# make build-site DIR=docs
+# go run ./cmd/geode/main.go build -dir docs
+make build-site DIR=docs
