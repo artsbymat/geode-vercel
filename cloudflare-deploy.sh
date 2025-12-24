6ae033e8a915cefb3f328219a2dbecd479a20993
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-12-25 00:30:19 +0700

    chore: add deploy script to cloudflare

---
+++ cloudflare-deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+GO_VERSION=1.25.5
+
+curl -LO https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
+tar -C /tmp -xzf go${GO_VERSION}.linux-amd64.tar.gz
+export PATH=/tmp/go/bin:$PATH
+
+go version
+make build-site DIR=docs
